dc7584e85783303fb3f34c5c5977692a8bf9637e
jiajiandong <jiandong@nfschina.com>
2014-12-26 16:40:25 +0800

    Unix s8

--- Unix/season8.threaduse/threaduse.c
+++ Unix/season8.threaduse/threaduse.c
@@ -4,10 +4,13 @@
 
 void rfork();
 void rvfork();
+void rwait();
+void rexec();
 
 main(){
 //	rfork();
-	rvfork();
+//	rvfork();
+	rwait();
 }
 
 void rfork(void){
@@ -15,11 +18,17 @@ void rfork(void){
 	ret = fork();
 	printf("ret = %d\n",ret);
 	if(ret == 0){
-		printf("ret == 0 %d, %d\n",getpid(),getppid());
+//		printf("ret == 0 %d, %d\n",getpid(),getppid());
+	exit(0);
 	}	
 	if(ret > 0){
-		printf("ret > 0 %d, %d\n",getpid(),getppid());
+//		printf("ret > 0 %d, %d\n",getpid(),getppid());
 	}
+	printf("after all %d\n",getpid());
+	
+	/**
+	子进程不只在if{}内有要执行的代码，如果在pid==0的{}里不使用exit退出，则子进程也不执行父进程的代码，因为子进程复制了这个父进程，代码段是相同的。但是其他字段就不共享。
+	**/
 }
 void rvfork(void){
 	int ret;
@@ -31,5 +40,51 @@ void rvfork(void){
 	}
 	printf("parent\n");
 }
+
+
+void rwait(){
+	int ret;
+	if( (ret = fork())<0 ){
+		printf("err fork \n");
+	}else if(ret == 0 ){ //child
+		printf("child sleep 5s\n");
+		sleep(5);
+	}else{	//parent
+		printf("parent waiting ...\n");
+		wait(ret);
+		printf("parent exit\n");
+	}
+
+	
+}
+
+void rexec(){
+	int ret;
+	if
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
