a3a238425ec4d8bc56e6a8e5d741a7fa258717c5
bycool <shenlie.06@163.com>
2018-04-15 23:29:59 +0800

    delete sth

--- kernel/relayfs-test/.relay_test.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/teanee/programming/kernel/relayfs-test/relay_test.ko := cp /home/teanee/programming/kernel/relayfs-test/relay_test.ko.unsigned /home/teanee/programming/kernel/relayfs-test/relay_test.ko
--- kernel/relayfs-test/.tmp_versions/relay_test.mod
+++
@@ -1,2 +0,0 @@
-/home/teanee/programming/kernel/relayfs-test/relay_test.ko
-/home/teanee/programming/kernel/relayfs-test/relay_test.o
