9cfcceb91d4940885334f51b9b9b47a9ef9f98e3
bycool <shenlie.06@163.com>
2019-04-25 18:15:49 -0700

    20190425

--- algs4/search/rbtree/rbtree.c
+++ algs4/search/rbtree/rbtree.c
@@ -1,13 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct node {
-	struct node* parent;
+typedef struct rbnode {
+	struct rbnode* parent;
 	int val;
-	int col;
-	struct node *left, *right;
+	int color;
+	struct rbnode *left, *right;
 }rbnode;
 
+typedef struct Tnode {
+	struct rbnode* root;
+}Tnode;
 
 
+/*     p             p
+ *     x             y
+ *  a     y       x    c
+ *       b c     a b  
+ */
 
+void left_rotate(Tnode* root, rbnode* x){
+	rbnode* y = x->right;
+
+	x->right = y->left;
+	if(y->left != NULL)
+		y->left->parent = x;
+
+	y->parent = x->parent;
+	if(x->parent == NULL){
+		root->root = y;
+	}else{
+		if(x->parent->left == x)
+			x->parent->left = y;
+		else
+			x->parent->right = y;
+	}
+	y->left = x;
+	x->parent = y;
+}
--- algs4/search/rbtree/rotate/rotatest.c
+++ algs4/search/rbtree/rotate/rotatest.c
@@ -4,6 +4,7 @@
 #define RED 1
 #define BLACK 0
 
+
 typedef struct node {
 	int val;
 	int color;
@@ -13,6 +14,8 @@ typedef struct node {
 typedef struct Troot {
 	node *root;
 }Troot;
+
+void rbtree_insert_fixup(Troot *root, node* node);
 
 /*
     z 	                         z
@@ -111,24 +114,110 @@ void rbtree_insert(Troot *root, node* new){
 	rbtree_insert_fixup(root, new);
 }
 
-void rbtree_insert_fixup(Troot *root, node* node){
-	node* parent, *gparent;
+void rbtree_insert_fixup(Troot *root, node* n){
+	printf("rbtree_insert_fixup: %d\n", n->val);
+	if(n->val == 60) return ;
+	node *parent, *gparent;
 
-	while((parent = node->parent) && (node->parent->color == RED)){
+	while((parent = n->parent) && (n->parent->color == RED)){
 		gparent = parent->parent;
 		if(parent == gparent->left){
-			{ // case1:
-				node* uncle = gparent->
+			// case1:
+			{
+				node* uncle = gparent->right;
+				if(uncle && uncle->color == RED){
+					uncle->color = BLACK;
+					parent->color = BLACK;
+					gparent->color = RED;
+					n = gparent;
+					continue;
+				}
 			}
+
+			// case2:
+			if(parent->right == n){
+				node* tmp ;
+				left_rotate(root, parent);
+				tmp = parent;
+				parent = n;
+				n = tmp;
+			}
+
+			// case3:
+			parent->color = BLACK;
+			gparent->color = RED;
+			right_rotate(root, gparent);
+		}else{
+			{
+				node* uncle = gparent->left;
+				if(uncle && uncle->color == RED){
+					uncle->color = BLACK;
+					parent->color = BLACK;
+					gparent->color = RED;
+					n = parent;
+					continue;
+				}
+			}
+
+			if(parent->left == n){
+				node* tmp;
+				right_rotate(root, parent);
+				tmp = parent;
+				parent = n;
+				n = tmp;
+			}
+
+			parent->color = BLACK;
+			gparent->color = RED;
+			left_rotate(root, gparent);
 		}
 	}
-
+	root->root->color = BLACK;
 }
 
+Troot* create_tree(){
+	Troot* root = (Troot*)malloc(sizeof(Troot));
+	root->root = NULL;
+	return root;
+}
+
+node* newnode(int val){
+	node* new = (node*)malloc(sizeof(node));
+	new->val = val;
+	new->parent = NULL;
+	new->left = NULL;
+	new->right = NULL;
+	return new;
+}
+
+void midisplay(node* root){
+	if(root == NULL) return ;
+	midisplay(root->left);
+	if(root->color == RED)
+		printf("[%d|R]", root->val);
+	else
+		printf("[%d|B]", root->val);
+	midisplay(root->right);
+}
+
+void midisplaytree(Troot* root){
+	if(root == NULL) return ;
+	midisplay(root->root);
+}
+
+void main(){
+	Troot* tree = create_tree();
+	int arr[20] = {10, 40, 30, 60, 90, 70, 20, 50, 80, 90, 100, 110, 120, 130,140, 150, 160, 170, 180, 190};
+	node* tmp = NULL;
+	int i = 0;
+	for(i=0; i<4; i++){
+		tmp = newnode(arr[i]);
+		rbtree_insert(tree, tmp);
+		printf("insert: %d\n", tmp->val);
+	}
+
+	midisplaytree(tree);
+	printf("\n");
 
 
-
-
-
-
-
+}
