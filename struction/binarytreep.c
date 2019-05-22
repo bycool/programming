abb7db11154d0b89967130c0b68425708160c658
bycool <shenlie.06@163.com>
2019-05-22 10:13:44 +0800

    20190522

--- struction/binarytreep.c
+++ struction/binarytreep.c
@@ -59,11 +59,11 @@ bnode* findmin(bnode* root){
 	return min;
 }
 
-void btree_delete_bnode(rbnode* root, int val){
+bnode* btree_delete_bnode(bnode* root, int val){
 	if(root == NULL) return ;
 
 	if(val < root->val){
-		root->left = btree_delete_bnode(root->left, val)
+		root->left = btree_delete_bnode(root->left, val);
 	}else if(val > root->val){
 		root->right = btree_delete_bnode(root->right, val);
 	}else if(root->left && root->right){
@@ -79,9 +79,61 @@ void btree_delete_bnode(rbnode* root, int val){
 			root = root->right;
 			root->right->parent = root->parent;
 		}
-		free(tmp;
+		free(tmp);
 	}
 	return root;
 }
 
+void btree_delete(btree* tree, int val){
+	if(tree == NULL) return ;
+	bnode* root = tree->root;
+	btree_delete_bnode(root, val);
+}
+
+void ldr_displaybnode(bnode* root){
+	if(root == NULL) return ;
+	ldr_displaybnode(root->left);
+	printf(".[%d]", root->val);
+	ldr_displaybnode(root->right);
+}
+
+void ldr_displaybtree(btree* tree){
+	if(tree == NULL) return ;
+	ldr_displaybnode(tree->root);
+	printf("\n");
+}
+
+void destorybnode(bnode* root){
+	if(root == NULL) return ;
+	destorybnode(root->left);
+	destorybnode(root->right);
+	printf("-[%d]", root->val);
+	free(root);
+}
+
+void destorybtree(btree* tree){
+	if(tree == NULL) return ;
+	destorybnode(tree->root);
+	free(tree);
+	printf("[-tree]\n");
+}
+
+void main(){
+	int arr[10] = {5,2,4,9,6,1,0,3,8,7};
+	int i = 0;
+	btree* tree = newbtree();
+	bnode* new = NULL;
+	for(i=0; i<10; i++){
+		new = newbnode(arr[i]);
+		btree_insert_bnode(tree, new);
+	}
+
+	ldr_displaybtree(tree);
+
+	btree_delete(tree, 9);
+
+	ldr_displaybtree(tree);
+
+	destorybtree(tree);
+}
 
