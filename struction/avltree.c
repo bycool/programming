f1f00ba8430b6ed4615b4801339fc44a9b8a55e0
bycool <shenlie.06@163.com>
2019-05-26 10:56:46 +0800

    2

--- struction/avltree.c
+++ struction/avltree.c
@@ -38,6 +38,25 @@ anode* left_left_rotate(anode* x){
 	return y;
 }
 
+//RR
+//              x
+//             / \
+//            a   y
+//               /  \
+//              b    c
+//                  /or\
+//                 e    d
+anode* right_right_rotate(anode* x){
+	anode *y = x->right;
+	x->right = y->left;
+	y->left = x;
+
+	x->height = MAX(HEIGHT(x->left), HEIGHT(x->right)) + 1;
+	y->height = MAX(HEIGHT(y->left), x->height) + 1;
+
+	return y;
+}
+
 //LR
 //             x
 //            / \
@@ -65,32 +84,68 @@ anode* right_left_rotate(anode* x){
 }
 
 
-//RR
-//              x
-//             / \
-//            a   y
-//               /  \
-//              b    c
-//                  /or\
-//                 e    d
-anode* right_right_rotate(anode* x){
-	anode *y = x->right;
-	x->right = y->left;
-	y->left = x;
 
-	x->height = MAX(HEIGHT(x->left), HEIGHT(x->right)) + 1;
-	y->height = MAX(HEIGHT(y->left), x->height) + 1;
 
-	return y;
+anode* avltree_insert(anode* root, int val){
+	if(root == NULL){
+		root = newanode(val);
+		return root;
+	}else if(val < root->val){
+		root->left = avltree_insert(root->left, val);
+		if(HEIGHT(root->left) - HEIGHT(root->right) == 2){
+			if(val < root->left->val)
+				root = left_left_rotate(root);
+			else
+				root = left_right_rotate(root);
+		}
+			
+	}else if(val > root->val){
+		root->right = avltree_insert(root->right, val);
+		if(HEIGHT(root->right) - HEIGHT(root->left) == 2){
+			if(val < root->right->val)
+				root = right_left_rotate(root);
+			else
+				root = right_right_rotate(root);
+			
+		}
+	}else{
+		printf("same val in avl tree\n");
+	}
+
+	root->height = MAX(HEIGHT(root->left), HEIGHT(root->right)) + 1;
+
+	return root;
 }
 
 
-anode* avltree_insert(anode* root, anode* new){
-	:
+void displaytree(anode* root){
+	if(root == NULL) return ;
+	displaytree(root->left);
+	printf(".[%d|%d]", root->val, root->height);
+	displaytree(root->right);
 }
 
+void destroytree(anode* root){
+	if(root == NULL) return ;
+	destroytree(root->left);
+	destroytree(root->right);
+	printf("-[%d|%d]", root->val, root->height);
+	free(root);
+}
 
+void main(){
+	int arr[16] = {3,2,1,4,5,6,7,16,15,14,13,12,11,10,8,9};
+	int i = 0;
+	anode* tree = NULL;
 
+	for(i=0; i<16; i++)
+		tree = avltree_insert(tree, arr[i]);
+
+	displaytree(tree);
+	printf("\n");
+	destroytree(tree);
+	printf("\n");
+}
 
 
 
