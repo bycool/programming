66473f7343f560340888aaf46a0be933a83adfe0
bycool <shenlie.06@163.com>
2019-05-30 09:39:42 +0800

    2

--- struction/listudg.c
+++ struction/listudg.c
@@ -34,7 +34,9 @@ Graph* create_graph(){
 
 	int i = 0;
 	char x, y;
+	int a, b;
 	enode* new = NULL;
+	enode* new2 = NULL;
 	enode* p = NULL;
 
 	char vexs[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G'};
@@ -65,41 +67,90 @@ Graph* create_graph(){
 		x = edgs[i][0];
 		y = edgs[i][1];
 
+		a = getxy(g, x);
+		b = getxy(g, y);
+
+		printf("a: %d, b: %d\n", a, b);
+
 		new = (enode*)malloc(sizeof(enode));
-		new->v = y;
+		new->v = b;
 		new->next = NULL;
 
-		p = g->vexs[x].next;
+		p = g->vexs[a].next;
 		if(p == NULL){
-			g->vexs[x].next = new;
+			g->vexs[a].next = new;
 		}else{
 			while(p->next)
 				p = p->next;
 			p->next = new;
 		}
+
+		new2 = (enode*)malloc(sizeof(enode));
+		new2->v = a;
+		new2->next = NULL;
+
+		p = g->vexs[b].next;
+		if(p == NULL){
+			g->vexs[b].next = new2;
+		}else{
+			while(p->next)
+				p = p->next;
+			p->next = new2;
+		}
 	}
 	return g;
 }
 
-void print_vnodelist(vnode* vn){
-	
+void print_vnodelist(vnode vn){
+	enode* p = NULL;
+	printf("[%c] ", vn.v);
+	p = vn.next;
+	while(p){
+		printf(" -> %d", p->v);
+		p = p->next;
+	}
+}
+
+void destoryvnodelist(vnode vn){
+	enode* p = NULL;
+	enode* dnode = NULL;
+
+	printf("[%c] ", vn.v);
+	p = vn.next;
+	while(p){
+		dnode = p;
+		p = p->next;
+		printf(" -[%d]", dnode->v);
+		free(dnode);
+	}
+	printf("\n");
 }
 
 
 void print_graph(Graph* g){
 	int i = 0;
 
+	printf("vexs: ");
 	for(i=0; i<g->vexnum; i++)
 		printf(" [%c] ", g->vexs[i]);
 	printf("\n");
 
-	
+	for(i=0; i<g->vexnum; i++){
+		printf("%d ", i);
+		print_vnodelist(g->vexs[i]);
+		printf("\n");
+	}
 }
 
 
 void main(){
+	int i = 0;
 	Graph* g = create_graph();
 	print_graph(g);
+
+	for(i=0; i<g->vexnum; i++)
+		destoryvnodelist(g->vexs[i]);
+
 	free(g);
 }
 
